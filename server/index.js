86b30e5f99406adf34efe5a9e3ea433a63e36760
msmsemanur <masumsemanur@gmail.com>
2022-01-05 21:51:00 +0300

    veritabanı bağlantısı

--- server/index.js
+++ server/index.js
@@ -8,3 +8,9 @@ const app = express()
 app.use(bodyParser.json({ limit: "32mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "32mb", extended: true }))
 app.use(cors())
+
+const CONNECTION_URL = 'mongodb+srv://admin:InstaLife@instalife.r9u84.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000
+mongoose.connect(CONNECTION_URL)
+   .then( ()=> app.listen(PORT, ()=> console.log('Server running on port:${PORT}'))  )
+   .catch(err=> console.log(err.message) )
