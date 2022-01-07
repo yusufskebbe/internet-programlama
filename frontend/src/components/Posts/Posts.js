d44f51c6a7c873263f63a7fadd63ed2e4ca6919e
yusuf <kebbeyusuf@gmail.com>
2022-01-08 01:06:35 +0300

    merge posts with post

--- frontend/src/components/Posts/Posts.js
+++ frontend/src/components/Posts/Posts.js
@@ -2,27 +2,29 @@ import React from 'react'
 import Post from './Post/Post'
 import useStyles from './Styles'
 import { useSelector } from 'react-redux'
-import {Grid, CircularProgress} from '@material-ui/core'
+import { Grid, CircularProgress } from '@material-ui/core'
+
+
 function Posts() {
 
-  
-    const classes = useStyles()
-    const posts =useSelector((state) => state.posts)
-  
+
+  const classes = useStyles()
+  const posts = useSelector((state) => state.posts)
+
 
   console.log(posts)
 
   return (
     !posts.length ? <CircularProgress /> :
-    <Grid className={classes.container} container alignItems="stretch" spacing={3} >
-      {posts.map(post => (
-        <Grid key={post._id} item xs={12} sm={6}>
-         <Post post = {post} />
-       </Grid>
-      ))
+      <Grid className={classes.container} container alignItems="stretch" spacing={3} >
+        {posts.map(post => (
+          <Grid key={post._id} item xs={12} sm={6}>
+            <Post post={post} />
+          </Grid>
+        ))
 
-      }
-    </Grid>
+        }
+      </Grid>
   );
 }
 
