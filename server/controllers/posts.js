071d5c11ce5362891bab43bd53458eb2b4be7754
yusuf <kebbeyusuf@gmail.com>
2022-01-09 17:14:53 +0300

    update and delete route bug fixed

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,53 +1,50 @@
-const PostMessage = require('../models/postMessage');
-const { post } = require('../routes/posts');
-const mongoose = 'mongoose'
+const PostMessage = require('../models/postMessage')
+
+const mongoose = require('mongoose')
 
 const getPosts = async (req, res) => {
     try {
-        const postMessage = await PostMessage.find()
-        console.log(postMessage);
+        const postMessages = await PostMessage.find()
 
-        res.status(200).json(postMessage)
+        res.status(200).json(postMessages)
     } catch (error) {
-        res.status(404).json({ Massage })
+        res.status(404).json({ message: error.message })
     }
-
 }
-const creatPost = async (req, res) => {
-    const body = req.body;
 
-    const newPost = new PostMessage(body)
+const createPost = async (req, res) => {
+    const post = req.body;
+
+    const newPost = new PostMessage({ ...post, creator: req.userId, createdAt: new Date().toISOString() })
 
     try {
         await newPost.save()
 
         res.status(201).json(newPost)
     } catch (error) {
-        res.status(409).json({ massage: error.massage })
+        res.status(409).json({ message: error.message })
     }
 }
 
-
-const updatePost = async(req,res) => {
-    const {id: _id } = req.params
-
+const updatePost = async (req, res) => {
+    const { id: _id } = req.params
     const post = req.body
 
-    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
 
-const updatePost  = await PostMessage.findByIdAndUpdate(_id, post, {new: true})
-res.json(updatePost)
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true })
 
+    res.json(updatedPost)
 }
-const deletePost = async (req,res) => {
-    const {id} = req.params
 
-    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
+const deletePost = async (req, res) => {
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
 
     await PostMessage.findByIdAndRemove(id);
-    
-    res.json({message: 'post deleted successfully' })
+
+    res.json({ message: 'post deleted successfully' })
 }
 
-module.exports = { getPosts, creatPost,updatePost,deletePost}
-
+module.exports = { createPost, deletePost, getPosts, updatePost }
--- server/index.js
+++ server/index.js
@@ -13,6 +13,10 @@ app.use(cors())
 
 app.use('/posts', postRoutes)
 
+app.get('/', (req, res) => {
+   res.send('Welcome to Instaverse API')
+})
+
 const CONNECTION_URL = 'mongodb+srv://admin:InstaLife@instalife.r9u84.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000
 mongoose.connect(CONNECTION_URL)
