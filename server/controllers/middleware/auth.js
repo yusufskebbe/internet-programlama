224041fe5b15253dc7ec31b210cdfd722794e792
sarahalkhater <sarahalhatir@gmail.com>
2022-01-10 01:06:58 +0300

    nd)

---
+++ server/controllers/middleware/auth.js
@@ -0,0 +1,22 @@
+
+const jwt = require ('jsonwebtoken')
+const auth = async (req, res, next) => {
+   try {
+      const token = req.headers.authorization.split(" ")[1];
+
+      let decodedData
+      if (token) {
+         decodedData = jwt.verify(token, 'test')
+
+         req.userId = decodedData?.id
+      }
+
+      next();
+
+   } catch (error) {
+      console.log(error);
+   }
+}
+
+
+module.exports =auth
