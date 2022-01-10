06d0b7d389637894522a85ecb9bd6cb0570f0290
yusuf <kebbeyusuf@gmail.com>
2022-01-10 19:45:53 +0300

    nothing

--- frontend/src/components/Navbar/styles.js
+++ frontend/src/components/Navbar/styles.js
@@ -1,53 +1,45 @@
-import {makeStyles} from '@material-ui/core/styles' ; 
+import { makeStyles } from '@material-ui/core/styles';
 import { deepPurple } from '@material-ui/core/colors';
 
-export default makeStyles ((theme) => ({
-
-appBar: {
-    borderRadius : 15,
-    margin : '30px 0 ',
-    display :'flex' , 
-    flexDirection : 'row' , 
-    justifyContent : 'space-between' , 
-    alignItems : 'center' , 
-    padding : '10px 50 px',
-},
-
-heading : {
-    color : 'rgba(0,183,255,1',
-    textDecoration : 'none' , 
-}, 
-image : {
-    display : 'flex' , 
-    justifyContent : 'flex-end' , 
-    width : '400px', 
-},
-toolbar: {
-    display : 'flex' , 
-    justifyContent : 'flex-end' , 
-    width : '400px',
-
-},
-profile : {
-    display :'flex',
-    justifyContent:'space-between',
-    width : '400px' ,
-},
-profile : {
-    display : 'flex' , 
-    justifyContent :'space-between',
-    width : '400px',
-},
-userName: {
-    display : 'flex', 
-    alignItems:'center',
-},
-brandContainer : {
-    display :'flex',
-    alignItems:'center',
-}, 
-purple :{
-    color : theme.palette.getContrastText(deepPurple[500]),
-    backgroundColor : deepPurple[500],
-},
+export default makeStyles((theme) => ({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 0',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#efefef'
+  },
+  heading: {
+    color: '#55133B',
+    fontWeight: '400',
+    textTransform: 'lowercase',
+    textDecoration: 'none'
+  },
+  image: {
+    marginLeft: '15px',
+  },
+  toolbar: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    width: '400px',
+  },
+  profile: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '400px',
+  },
+  userName: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  brandContainer: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  purple: {
+    color: theme.palette.getContrastText(deepPurple[500]),
+    backgroundColor: deepPurple[500],
+  },
 }));
